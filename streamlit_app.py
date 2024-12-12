1430280aaf62a8324ec28aba1a82602ae30ee264
Goyo <goyodiaz@gmail.com>
2024-12-12 10:36:32 +0100

    Use only Squat

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ for target in ['Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg']:
 
 # Form for user input
 with st.form(key="lift_prediction_form"):
-    input_lift = st.selectbox("Which lift do you want to predict?", ["Squat", "Bench", "Deadlift"])
+    input_lift = "Squat"
     bodyweight = st.number_input("Enter your body weight (kg):", min_value=20.0, max_value=200.0, step=0.1)
     sex = st.selectbox("Select your sex:", ["Male", "Female"])
     sex_encoded = 0 if sex == "Male" else 1
@@ -40,12 +40,6 @@ with st.form(key="lift_prediction_form"):
     if input_lift == "Squat":
         bench = st.number_input("Enter your Bench (kg):", min_value=20.0, max_value=600.0, step=0.1, key="bench_input")
         deadlift = st.number_input("Enter your Deadlift (kg):", min_value=20.0, max_value=600.0, step=0.1, key="deadlift_input")
-    elif input_lift == "Bench":
-        squat = st.number_input("Enter your Squat (kg):", min_value=20.0, max_value=600.0, step=0.1, key="squat_input")
-        deadlift = st.number_input("Enter your Deadlift (kg):", min_value=20.0, max_value=600.0, step=0.1, key="deadlift_input")
-    elif input_lift == "Deadlift":
-        squat = st.number_input("Enter your Squat (kg):", min_value=20.0, max_value=600.0, step=0.1, key="squat_input")
-        bench = st.number_input("Enter your Bench (kg):", min_value=20.0, max_value=600.0, step=0.1, key="bench_input")
 
     # Submit button
     submit = st.form_submit_button(label="Predict")
@@ -55,9 +49,3 @@ if submit:
     if input_lift == "Squat":
         predicted_value = models['Best3SquatKg'].predict([[bench, deadlift, bodyweight, sex_encoded]])[0]
         st.write("Predicted Squat:", round(predicted_value, 2), "kg")
-    elif input_lift == "Bench":
-        predicted_value = models['Best3BenchKg'].predict([[squat, deadlift, bodyweight, sex_encoded]])[0]
-        st.write("Predicted Bench:", round(predicted_value, 2), "kg")
-    elif input_lift == "Deadlift":
-        predicted_value = models['Best3DeadliftKg'].predict([[squat, bench, bodyweight, sex_encoded]])[0]
-        st.write("Predicted Deadlift:", round(predicted_value, 2), "kg")
